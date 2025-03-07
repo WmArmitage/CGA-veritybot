7ab8cbedbcc06bf05f7aee904b99f202c4d0b600
William Armitage <wm.armitage@gmail.com>
2025-03-06 21:35:54 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -123,7 +123,7 @@ if member and role:
 else:
     await interaction.response.send_message("User or role not found.", ephemeral=True)
 
- elif interaction.data and interaction.data["custom_id"].startswith("decline_"):
+    elif interaction.data and interaction.data["custom_id"].startswith("decline_"):
         custom_id = interaction.data
         _, user_id, role_id = custom_id.split("_") #from this line down is a guess
         user_id = int(user_id)
