74bb797cc75b7eb9dddc81e10de155cf17772141
William Armitage <wm.armitage@gmail.com>
2025-03-07 19:25:35 -0500

    Update bot.py
    added on line 157 
    admin_role = discord.utils.get(interaction.guild.roles, id=ADMIN_ROLE_ID)
    
    because of syntax error

--- bot.py
+++ bot.py
@@ -154,7 +154,7 @@ class DeclineReasonModal(discord.ui.Modal):
 
 @bot.command() 
 async def viewdb(interaction: discord.Interaction):
-    admin_role = discord.utils.get(interaction: discord.Interaction.guild.roles, id=ADMIN_ROLE_ID)
+    admin_role = discord.utils.get(interaction.guild.roles, id=ADMIN_ROLE_ID)
     if admin_role not in interaction: discord.Interaction.author.roles:
         await interaction: discord.Interaction.send("You do not have permission to view the database.")
         return
